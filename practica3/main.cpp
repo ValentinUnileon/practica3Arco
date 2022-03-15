65a269f6ef87f4e616bb2987a2cd4a0f3fb3b73a
ValentinUnileon <vsantp01@estudiantes.unileon.es>
2022-03-15 20:21:42 +0100

    prueba de commit desde QT

--- practica3/main.cpp
+++ practica3/main.cpp
@@ -1,11 +1,17 @@
 #include "menu.h"
 
 #include <QApplication>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     menu w;
     w.show();
+    cout<<"hola que tal";
     return a.exec();
+
 }
