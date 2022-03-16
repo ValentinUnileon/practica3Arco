9a4302e30b94949379f361f6c488419897586433
ValentinUnileon <vsantp01@estudiantes.unileon.es>
2022-03-16 20:06:35 +0100

    commit de tranquilidad

--- practica3/main.cpp
+++ practica3/main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     menu aaa;
     aaa.show();
+
+    //modificacion con suavidad
     cout<<"bien";
     cout<<"asdfasdf";
     cout<<"asdfasdf mi nombre es GERARDO OLA K TAL CALLATE :) POR FABOR BORRAD HESTO GRAZIAS";
